ba0d37d84cc118b7df59bc27915653385bb536b2
Egor <40001420+EgorLedyaev@users.noreply.github.com>
2021-09-29 20:31:56 +0400

    Add files via upload

---
+++ parser_ege/spiders/ege_spider.py
@@ -0,0 +1,29 @@
+import scrapy
+
+print("Результат программы будет в файле 'dump.json'")
+
+first_id = int(input("Введите начальный id теста"))
+last_id = int(input("Введите конечный id теста"))
+name_teacher = input("Введите Имя и Фамилию преподавателя")
+
+class CatalogSpider(scrapy.Spider):
+    name = 'catalog'
+    allowed_domains = ['inf-ege.sdamgia.ru']
+    start_urls = ['https://inf-ege.sdamgia.ru/']
+    pages_count = last_id
+
+
+    def start_requests(self):
+        for page in range(first_id, 1 + self.pages_count):
+            url = f'https://inf-ege.sdamgia.ru/test?id={page}'
+            yield scrapy.Request(url, callback=self.parse)
+
+    def parse(self, response):
+
+        item = {
+            'url': response.request.url,
+            'title': response.css('div.new_header b').extract_first('').strip().replace('</b>','').replace('<b>','').replace('<!--np-->',''),
+        }
+
+        if str(item)[-19:-2] == str(name_teacher):
+            yield item
